57e0679f7ba0ed0cfba89af65f70c21d2f78aede
pard1s <76449700+pard1s@users.noreply.github.com>
2025-09-08 16:48:10 -0500

    Initial commit: Express.js server setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log("server is running on http://localhost:5000");
+});
+
+
+app.get("/", (req, res) => {
+    res.send("hello world!");
+})
